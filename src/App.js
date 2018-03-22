a6d03f0e74b5099ee615d90ba0b6f200e52e49b6
valePV <va.perez19@hotmail.com>
2018-03-22 17:30:18 -0300

    component button

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Buttons from './components/Buttons';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Laboratoria Style Guide</h1>
+        </header>
+        <Buttons></Buttons>
+        
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+
